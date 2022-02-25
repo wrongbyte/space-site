5c2d7cf9b9a0a900a463ad641f68981581eb1649
wrongbyte <57643375+wrongbyte@users.noreply.github.com>
2022-02-25 16:36:40 -0300

    add active-page border navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,10 +13,13 @@ export default function Navbar() {
 
             <div className='navbar-background'></div>
             <div className='pages-wrapper'>
-                <span id='home' className='nav-item'><b>00</b><span className='page-name'>HOME</span></span>
+                <span id='home' className='nav-item active-page'><b>00</b><span className='page-name'>HOME</span></span>
                 <span id='destination' className='nav-item'><b>01</b> <span className='page-name'>DESTINATION</span></span>
+                
                 <span id='crew' className='nav-item'><b>02</b><span className='page-name'>CREW</span></span>
+                
                 <span id='technology' className='nav-item'><b>03</b><span className='page-name'>TECHNOLOGY</span></span>
+
             </div>
         </div>
     )
