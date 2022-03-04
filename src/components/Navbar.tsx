72c2e5f26f29b9aea4a0aad120fe1c4891e40075
wrongbyte <57643375+wrongbyte@users.noreply.github.com>
2022-03-04 13:46:31 -0300

    finish navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,11 @@
 import './navbar.css';
 import logo from  '../assets/shared/logo.svg';
-import { Link } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 
 export default function Navbar() {
+    const location = useLocation();
+    const pathname = location.pathname;
+
     return (
         <div className='navbar-div'>
             <div className='wrapper'>
@@ -13,15 +16,16 @@ export default function Navbar() {
             </div>
 
             <div className='navbar-background'></div>
+            
             <div className='pages-wrapper'>
                 
-                <span id='home' className='nav-item active-page'><b>00</b><span className='page-name'><Link to='/'>HOME</Link></span></span>
+                <span id='home' className={`nav-item ${pathname === '/' && 'active-page'}`}><b>00</b><span className='page-name'><Link to='/'>HOME</Link></span></span>
                 
-                <span id='destination' className='nav-item'><b>01</b><span className='page-name'><Link to='/destination'>DESTINATION</Link></span></span>
+                <span id='destination' className={`nav-item ${pathname === '/destination' && 'active-page'}`}><b>01</b><span className='page-name'><Link to='/destination'>DESTINATION</Link></span></span>
                 
-                <span id='crew' className='nav-item'><b>02</b><span className='page-name'><Link to='/crew'>CREW</Link></span></span>
+                <span id='crew' className={`nav-item ${pathname === '/crew' && 'active-page'}`}><b>02</b><span className='page-name'><Link to='/crew'>CREW</Link></span></span>
                 
-                <span id='technology' className='nav-item'><b>03</b><span className='page-name'><Link to='/technology'>TECHNOLOGY</Link></span></span>
+                <span id='technology' className={`nav-item ${pathname === '/technology' && 'active-page'}`}><b>03</b><span className='page-name'><Link to='/technology'>TECHNOLOGY</Link></span></span>
 
             </div>
         </div>
