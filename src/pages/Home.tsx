5c250e2d11402d7fc3e38c66a6cffc988386e608
wrongbyte <57643375+wrongbyte@users.noreply.github.com>
2022-03-07 16:06:54 -0300

    tablet view initial layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './home.css';
 export default function Home() {
     return (
@@ -8,7 +9,7 @@ export default function Home() {
                     <p className='space-paragraph'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
                 </div>
 
-                <div id='circle'><span className='explore'>EXPLORE</span></div>
+                <div id='circle'><span className='explore'><Link to='/destination'>EXPLORE</Link></span></div>
         </main>
     )
 }
