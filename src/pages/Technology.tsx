ac440c194889c538635242990f1a161f9e1847b5
wrongbyte <57643375+wrongbyte@users.noreply.github.com>
2022-03-08 14:18:53 -0300

    technology tablet design

--- src/pages/Technology.tsx
+++ src/pages/Technology.tsx
@@ -7,33 +7,36 @@ export default function Technology() {
     const getTechnologyIndex = (e: any, value: number) => setTechnologyIndex(value);
     return (
         <main className='technology'>
-                        <span className='page-title'><span className='page-number'>03</span> SPACE LAUNCH 101</span>
-
-                        <section className='technology-info'>
-
-                        <nav className='technology-nav'>
-                        {
-                                data.technology.map((technology, index) => {
-                                    if (index === technologyIndex) {
-                                        return (
-                                            <div onClick={ (event) => getTechnologyIndex(event, index) } key={index} className='technology-nav-button-active'>{index}</div>
-                                        )
-                                    }
-                                    return (
-                                       <div onClick={ (event) => getTechnologyIndex(event, index) } key={index} className='technology-nav-button'>{index}</div>
+            <span className='page-title'><span className='page-number'>03</span> SPACE LAUNCH 101</span>
+            <div className='technology-wrapper'>
+                <section className='technology-info'>
+                    <nav className='technology-nav'>
+                    {
+                        data.technology.map((technology, index) => {
+                            if (index === technologyIndex) {
+                                return (
+                                    <div onClick={ (event) => getTechnologyIndex(event, index) } key={index} className='technology-nav-button-active'>{index}</div>
+                                    )
+                                }
+                                return (
+                                    <div onClick={ (event) => getTechnologyIndex(event, index) } key={index} className='technology-nav-button'>{index}</div>
                                     )
                                 })
-                        }
-                        </nav>
+                    }
+                    </nav>
 
-                        <div className='technology-text'>
-                            <span className='terminology'>THE TERMINOLOGY…</span>
-                            <span className='technology-name'>{data.technology[technologyIndex].name}</span>
-                            <span className='technology-description'>{data.technology[technologyIndex].description}</span>
-                        </div>
-                        </section>
+                    <div className='technology-text'>
+                        <span className='terminology'>THE TERMINOLOGY…</span>
+                        <span className='technology-name'>{data.technology[technologyIndex].name}</span>
+                        <span className='technology-description'>{data.technology[technologyIndex].description}</span>
+                    </div>
+                </section>
+                <picture>
+                    <source media='(max-width:1020px)' srcSet={data.technology[technologyIndex].images.landscape}/>
+                    <img className='technology-image' src={data.technology[technologyIndex].images.portrait}/>
+                </picture>
 
-                        <img className='technology-image' src={data.technology[technologyIndex].images.portrait}></img>
+            </div>
         </main>
     )
 }
